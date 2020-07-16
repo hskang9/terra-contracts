a629fa10cc3b4e97a5fe1ee05d216b6c9d1de8d2
Hyungsuk Kang <cai@terra.money>
2020-07-17 08:51:48 +0900

    update Cargo.toml

---
+++ erc721/target/rls/debug/examples/schema-f8c2a80e95175267.d
@@ -0,0 +1,5 @@
+/Users/hyungsukkang/terra/terra-contracts/erc721/target/rls/debug/examples/schema-f8c2a80e95175267.rmeta: examples/schema.rs
+
+/Users/hyungsukkang/terra/terra-contracts/erc721/target/rls/debug/examples/schema-f8c2a80e95175267.d: examples/schema.rs
+
+examples/schema.rs:
