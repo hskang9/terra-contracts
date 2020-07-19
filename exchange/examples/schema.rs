cd0cfe8cc54b46e81d7280bf928e15663ec8db5c
hskang9 <hskang9@gmail.com>
2020-07-20 06:18:51 +0900

    update schema

--- exchange/examples/schema.rs
+++ exchange/examples/schema.rs
@@ -3,8 +3,8 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use uniswap_v1::msg::*;
-use uniswap_v1::state::*;
+use exchange::msg::*;
+use exchange::state::*;
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
